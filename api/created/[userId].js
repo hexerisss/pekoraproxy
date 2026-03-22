0b29ad3c4a550049960edee6e4fd211ae7cc6c71
hexerisss <twopunchman24@gmail.com>
2026-03-22 17:30:22 +0100

    Create [userId].js

---
+++ api/created/[userId].js
@@ -0,0 +1,16 @@
+import { getUserCreatedItems } from '../../assets';
+
+export default async function handler(req, res) {
+    const { userId } = req.query;
+
+    if (!userId || isNaN(userId)) {
+        return res.status(400).json({ error: "Invalid userId" });
+    }
+
+    try {
+        const items = await getUserCreatedItems(userId);
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch created items" });
+    }
+}
